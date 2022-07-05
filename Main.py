9c25534e10135171a0c23fe4f559ff9f169782d3
Sai <sai.rj@somaiya.edu>
2022-07-05 23:23:22 +0530

    Enemy added
    imported random module and added enemy

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 
 pygame.init()
@@ -24,8 +24,27 @@ playerX=370
 playerY=480
 playerX_change=0
 
+#Enemy
+EnemyImg = []
+EnemyX = []
+EnemyY = []
+EnemyX_change = []
+EnemyY_change = []
+num_of_enemies = 4
+for i in range(num_of_enemies) :
+    EnemyImg.append(pygame.image.load('Enemy.png'))
+    EnemyX.append(random.randint(0,735))
+    EnemyY.append(random.randint(50,150))
+    EnemyX_change.append(1)
+    EnemyY_change.append(40)
+
 def player(x,y):
     screen.blit(playerImg,(x,y))
+
+def Enemy(x,y,i):
+    screen.blit(EnemyImg[i],(x,y))
+
+
 
 # Game loop
 running = True
